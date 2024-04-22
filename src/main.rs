9f656e16681016f65176bf28498b7f6486cc81bb
Sheldon <Sheldon.Wei.-ND@disney.com>
2024-04-22 13:45:53 +0800

    v 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,45 @@
 use fltk::{app, prelude::*, window};
+use fltk::enums::Color;
 use fltk::frame::Frame;
 use fltk::group::{Pack, PackType};
 
 fn main() {
     let app = app::App::default();
-    let mut wind = window::Window::default().with_size(400, 500).with_label("000000000000000000");
+    let mut wind = window::Window::default().with_size(400, 500).with_label("Multi-Row Multi-Column Layout");
     // let mut wind = window::Window::new(100, 100, 400, 400, "Multi-Row Multi-Column Layout");
 
     // 创建一个 4x4 的网格布局
-    let mut group = Pack::new(10, 10, 380, 380, "grid layout");
-    group.set_type(PackType::Horizontal);
-    group.set_spacing(10);
+    let mut grid_pack = Pack::new(10, 10, 38, 38, "grid layout");
+    grid_pack.set_type(PackType::Horizontal);
+    grid_pack.set_spacing(40);
 
-    for _ in 0..4 {
-        let mut row = Pack::new(0, 0, 380, 95, "2222222222222222222");
-        row.set_type(PackType::Vertical);
-        row.set_spacing(10);
+    for i in 0..4 {
+        let mut column_pack = Pack::default().with_size(100, 200).with_label(&*format!("列 {i}"));
+        column_pack.set_type(PackType::Vertical);
+        column_pack.set_spacing(50);
+        column_pack.auto_layout();
 
-        for _ in 0..4 {
-            let mut frame = Frame::new(0, 0, 95, 95, "33333333333333333333");
-            frame.set_color(fltk::enums::Color::from_rgb(
+        for j in 0..4 {
+            let mut frame = Frame::default().with_size(100, 200).with_label(&*format!("{i} j {j}"));
+            frame.set_color(Color::from_rgb(
                 rand::random::<u8>(),
                 rand::random::<u8>(),
                 rand::random::<u8>(),
             ));
-            row.end();
-            row.add(&frame);
+            column_pack.end();
+            column_pack.add(&frame);
         }
 
-        group.end();
-        group.add(&row);
+        grid_pack.end();
+        grid_pack.add(&column_pack);
     }
 
     wind.end();
     wind.make_resizable(true);
     wind.fullscreen(true);
     wind.show();
+    wind.set_callback(move |_| {
+        grid_pack.redraw();
+    });
     app.run().unwrap();
 }
