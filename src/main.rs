b2800571e145d79f942a9ec25ea569872de33458
Sheldon <Sheldon.Wei.-ND@disney.com>
2024-04-22 14:05:49 +0800

    v 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,55 @@
 use fltk::{app, prelude::*, window};
+use fltk::app::screen_size;
 use fltk::enums::Color;
 use fltk::frame::Frame;
 use fltk::group::{Pack, PackType};
 
 fn main() {
     let app = app::App::default();
-    let mut wind = window::Window::default().with_size(400, 500).with_label("Multi-Row Multi-Column Layout");
-    // let mut wind = window::Window::new(100, 100, 400, 400, "Multi-Row Multi-Column Layout");
+    let (width, height) = screen_size();
+    println!("screen {} {}", width, height);
+    let width = width as i32;
+    let height = height as i32;
+    let mut wind = window::Window::default().with_size(width, height).with_label("Multi-Row Multi-Column Layout");
 
+    let col_count = 4;
+    let row_count = 3;
     // 创建一个 4x4 的网格布局
-    let mut grid_pack = Pack::new(10, 10, 38, 38, "grid layout");
+    let mut grid_pack = Pack::new(0, 0, width, height, "grid layout");
     grid_pack.set_type(PackType::Horizontal);
-    grid_pack.set_spacing(40);
+    grid_pack.set_spacing(10);
 
-    for i in 0..4 {
-        let mut column_pack = Pack::default().with_size(100, 200).with_label(&*format!("列 {i}"));
+    for i in 0..col_count {
+        let mut column_pack = Pack::default().with_size(wind.width() / col_count, wind.height()).with_label(&*format!("列 {i}"));
         column_pack.set_type(PackType::Vertical);
-        column_pack.set_spacing(50);
-        column_pack.auto_layout();
+        column_pack.set_spacing(10);
+        // column_pack.auto_layout();
+        column_pack.set_color(Color::from_rgb(
+            rand::random::<u8>(),
+            rand::random::<u8>(),
+            rand::random::<u8>(),
+        ));
 
-        for j in 0..4 {
-            let mut frame = Frame::default().with_size(100, 200).with_label(&*format!("{i} j {j}"));
-            frame.set_color(Color::from_rgb(
-                rand::random::<u8>(),
-                rand::random::<u8>(),
-                rand::random::<u8>(),
-            ));
+
+        for j in 0..row_count {
+            let mut frame = Frame::default().with_size(column_pack.width(), column_pack.height() / row_count).with_label(&*format!("{i} j {j}"));
             column_pack.end();
             column_pack.add(&frame);
+
+            column_pack.set_callback(move |_| frame.redraw())
         }
 
         grid_pack.end();
         grid_pack.add(&column_pack);
+
+        grid_pack.set_callback(move |_| {
+            column_pack.redraw();
+        })
     }
 
     wind.end();
     wind.make_resizable(true);
-    wind.fullscreen(true);
+    // wind.fullscreen(true);
     wind.show();
     wind.set_callback(move |_| {
         grid_pack.redraw();
