20be39063a07b825ba0245a081a517a44cecd640
Sheldon <Sheldon.Wei.-ND@disney.com>
2024-04-30 11:42:49 +0800

    v 0.5.1

--- src/component/structure_tree.rs
+++ src/component/structure_tree.rs
@@ -1,5 +1,6 @@
+use fltk::enums::Color;
 use fltk::prelude::WidgetExt;
-use fltk::tree::Tree;
+use fltk::tree::{Tree, TreeItem, TreeSelect};
 
 pub(crate) struct JsonStructure {
     view: Box<Tree>
@@ -7,9 +8,26 @@ pub(crate) struct JsonStructure {
 
 impl JsonStructure {
     pub(crate) fn new(w: i32, h: i32) -> Self {
-        let mut t = Tree::default().with_size(w, h);
+        let mut tree = Tree::default().with_size(w, h);
+        tree.set_root_label(".");
+        tree.set_select_mode(TreeSelect::Multi);
+        tree.set_color(Color::Blue);
+        for i in 0 .. 5 {
+            // t.add(&*("item".to_owned() + &*i.to_string()));
+            let ti = TreeItem::new(&tree, &*("item".to_owned() + &*i.to_string()));
+            tree.add_item("2/2/3", &ti);
+        }
+
+        tree.set_callback(|t| {
+            if let Some(items) = t.get_selected_items() {
+                for i in items {
+                    println!("{} selected", t.item_pathname(&i).unwrap());
+                }
+            }
+        });
+
         JsonStructure {
-            view: Box::new(t)
+            view: Box::new(tree)
         }
     }
 
